80dcd8c7bb23d1c5053c162eeac248693475e631
Rete-Keyt <retekeyt@gmail.com>
2015-09-23 10:53:14 +0300

    HTML5 boilerplate

---
+++ kliendipoolsed.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: Rex
+ * Date: 23/09/2015
+ * Time: 10:50
+ */
