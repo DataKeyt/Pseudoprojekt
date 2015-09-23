2b900f45636bfd2dc3cdb5a1474909848eb82205
Rete-Keyt <retekeyt@gmail.com>
2015-09-23 11:03:14 +0300

    2. Tere maailm button
    [Delivers #103973990]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,10 +5,11 @@
     <title>Document</title>
 </head>
 <body>
+<button type="button" onclick="alert('Tere maailm!')">Tere maailm!</button>
 </body>
 </html>
-/**
+<!--
  * Created by PhpStorm.
  * Date: 23/09/2015
  * Time: 10:50
- */
+ -->
