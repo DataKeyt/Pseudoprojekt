c0e0f74a4cdbe0dd7641ddbbe3a98be9b5955255
DataKeyt <retekeyt@gmail.com>
2015-10-17 14:37:09 +0300

    17. Tingimused
    [Delivers #103974020]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -14,6 +14,11 @@ $person = array(
 //Boolean
 $eesnimi_algab_vokaaliga = false;
 
+//Condition
+if ($eesnimi_algab_vokaaliga) {
+    echo "<p>Nimi algab vokaaliga</p>";
+};
+
 echo "<p>" . $person['firstname'] . " " . $person['lastname'] . " (" . $person['age'] . ")</p>";
 
 ?>
