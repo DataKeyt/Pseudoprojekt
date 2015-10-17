f64c8058872e0ecff11bf1a8eb7bdb70d09d860d
DataKeyt <retekeyt@gmail.com>
2015-10-17 14:23:13 +0300

    15. Boolean
    [Delivers #103974016]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -5,8 +5,15 @@
  * Date: 17.10.2015
  * Time: 13:41
  */
+
+//Strings
 $firstname = "Rete-Keyt";
 $lastname = "Uibo";
+
+//Integer
 $age = 19;
+
+//boolean
+$eesnimi_algab_vokaaliga = false;
 
 echo $firstname . " " . $lastname . " ($age)";
