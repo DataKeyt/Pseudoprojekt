dbd4991207fd9c0162ed32ac90d7cae87c30b45b
DataKeyt <retekeyt@gmail.com>
2015-10-17 13:45:11 +0300

    13. Stringid
    [Delivers #103974012]

---
+++ serveripoolsed.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DataKeyt
+ * Date: 17.10.2015
+ * Time: 13:41
+ */
+$firstname = "Rete-Keyt";
+$lastname = "Uibo";
+
+echo $firstname . " " . $lastname;
