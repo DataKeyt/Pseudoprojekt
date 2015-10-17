2b20f2c0e5293b43f44376bf2d6eea72455d6606
DataKeyt <retekeyt@gmail.com>
2015-10-17 19:45:16 +0300

    31. Andmebaasist andmete võtmine
    [Delivers #105916438]

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 //Connect database
-$db = mysqli_connect('localhost', 'root', '', 'blog') or die(mysqli_error($db));
+require_once 'config.php';
+$db = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
 
 //Retrieve data from database
